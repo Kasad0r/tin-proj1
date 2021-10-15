c008e2384bcdf381cc9442101f2606fb1846cade
kasad0r <pokemon071199>
2021-10-15 11:16:09 +0200

    Add something

--- js/pages/movie/script.js
+++ js/pages/movie/script.js
@@ -16,3 +16,17 @@ function successfulAdded(){
 function successfullyModified(){
     alert("Movie successfully modified")
 }
+
+
+let expanded = false;
+
+function showCheckboxes() {
+    var checkboxes = document.getElementById("checkboxes");
+    if (!expanded) {
+        checkboxes.style.display = "block";
+        expanded = true;
+    } else {
+        checkboxes.style.display = "none";
+        expanded = false;
+    }
+}
